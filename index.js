c913e462d1d650d051a603d04b2afc0c0f80fa7c
Matthew Bomett <bomettmat2@gmail.com>
2023-09-21 00:37:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+const arrayOfFirstDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const firstTwoDrivers = returnFirstTwoDrivers(arrayOfFirstDrivers);
+console.log(firstTwoDrivers);
+
+const returnLastTwoDrivers = function(lastDrivers){
+    return lastDrivers.slice(2);
+};
+
+const arrayOfLastDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const lastDrivers = returnLastTwoDrivers(arrayOfLastDrivers);
+console.log(lastDrivers)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier){
+    return function (fare){
+        return fare * multiplier;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(drivers, selectingDrivers){
+    return selectingDrivers(drivers);
+}
+
+const arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+console.log(returnFirstTwoDrivers(arrayOfDrivers));
+console.log(returnLastTwoDrivers(arrayOfDrivers));
